ed2cbc236599aad490de8c26a9a1ac41661340e7
Aradhya Shukla <aradhya1703@gmail.com>
2024-07-06 16:48:07 +0530

    error correction

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -39,7 +39,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
